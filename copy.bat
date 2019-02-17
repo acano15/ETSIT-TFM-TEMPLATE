63add0c999685ac672822b70df2d5a0fe709e92b
acano15 <antonio.cano.montes@hotmail.com>
2019-02-17 15:04:30 +0100

    Add files via upload

---
+++ copy.bat
@@ -0,0 +1,6 @@
+
+rd /s /q "...\TemplateETSIT_TFM"
+
+mkdir "...\TemplateETSIT_TFM_Copy"
+
+xcopy "C:\Users\GAPS\Desktop\ANTONIO\TFM\ToWrite\TFM-Template" "C:\Users\GAPS\Desktop\copiar\COPIATFM" /E /I
